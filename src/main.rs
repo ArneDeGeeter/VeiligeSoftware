efd41284fabe18f9ad698ddec375a0494b66b314
Arne De geeter <arne.de.geeter0@gmail.com>
2020-07-15 12:00:15 +0200

    We are very good at rust

--- src/main.rs
+++ src/main.rs
@@ -271,7 +271,7 @@ impl Timer {
 
     fn new() -> Timer {
         let mut var=(BCM2709_PERI_BASE+TIMER_REGISTER_OFFSET) as *const u32;
-        let mut var2=(var+4) as *mut u32;
+        let mut var2=(var+0x4) as *mut u32;
         Timer { _timemap: None, timereg: var2 }
         // TODO: Implement this yourself.
     }
