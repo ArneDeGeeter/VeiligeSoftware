67afbe38687200d9017856729faaa1d12c66dae6
Arne De geeter <arne.de.geeter0@gmail.com>
2020-07-27 14:46:17 +0200

    :?

--- src/main.rs
+++ src/main.rs
@@ -226,7 +226,7 @@ impl GPIO {
         unsafe { *pinOutputSet = *pinOutputSet | *bitmask }
         println!("{:#034b}", unsafe { *pinOutputSet });
         println!("{:#034b}", unsafe { *pinOutputClear });
-        println!("{},adr",self.gpio_read_bits_);
+        println!("{:?},adr",self.gpio_read_bits_);
         println!("{:#034b}", unsafe { *self.gpio_read_bits_ });
 
 
