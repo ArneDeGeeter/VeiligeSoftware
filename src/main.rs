b5274864fc5dcfea08d2f726956f061cd106e839
Arne De geeter <arne.de.geeter0@gmail.com>
2020-07-29 14:19:47 +0200

    123

--- src/main.rs
+++ src/main.rs
@@ -438,7 +438,6 @@ pub fn read_bmp() -> Result<Image, std::io::Error> { // {
     };
     image.pixels = vec![vec![Pixel { r: 0, g: 0, b: 0 }; image.width as usize]; image.height as usize];
     for (i, j) in img.coordinates() {
-        println!("{} {}",i,j);
         image.pixels[j as usize][i as usize] = Pixel { r: img.get_pixel(i as u32, j as u32).r as u16, g: img.get_pixel(i as u32, j as u32).g as u16, b: img.get_pixel(i as u32, j as u32).b as u16 }
     }
 
