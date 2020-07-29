7f7fb79c85535119bd853851c99212176620d4c1
Arne De geeter <arne.de.geeter0@gmail.com>
2020-07-29 12:45:32 +0200

    interrupt

--- src/main.rs
+++ src/main.rs
@@ -241,6 +241,11 @@ impl GPIO {
         self.activatePins(&mut ((GPIO_BIT!(PIN_OE)) as u32));
     }
     fn showImage(self: &mut GPIO, image: &Image) {
+        let interrupt_received = Arc::new(AtomicBool::new(false));
+        let int_recv = interrupt_received.clone();
+        ctrlc::set_handler(move || {
+            int_recv.store(true, Ordering::SeqCst);
+        }).unwrap();
         while interrupt_received.load(Ordering::SeqCst) == false {
             for x in 0usize..8 {
                 let rowMask = match x {
@@ -629,3 +634,4 @@ pub fn main() {
 
 // TODO: You may want to reset the board here (i.e., disable all LEDs)
 }
+
